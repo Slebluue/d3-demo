20ed91ef7b3d01a5e021ba7ea7b11eb3f037e021
slebluue <mitchell8morrison@gmail.com>
2023-10-04 14:20:42 -0700

    minor cleanup of background styles

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ margin: 0 }}>
+      <body className={inter.className} style={{ margin: 0, height: '100vh', background: '#f6f8fa' }}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <Analytics />
       </body>
